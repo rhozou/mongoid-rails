67c67569c46a6770390ea0ab5077923c8372fddb
Xiangjin Zou <xzou@squareup.com>
2019-05-13 18:30:48 -0700

    Fix selectable for rails 5

--- lib/mongoid-rails/forbidden_query_protection.rb
+++ lib/mongoid-rails/forbidden_query_protection.rb
@@ -1,7 +1,7 @@
 module MongoidRails
   module ForbiddenQueryProtection
     # Redefine all query methods to be safe against hash injection attacks.
-    Origin::Selectable.instance_methods(false).each do |method|
+    ::Mongoid::Criteria::Queryable::Selectable.instance_methods(false).each do |method|
       define_method method do |*criteria|
         raise ActiveModel::ForbiddenAttributesError unless MongoidRails.should_permit?(criteria)
         super(*criteria)
